251c50e778f0c0fb4acf4551e2a1b81940d713bd
claudinemahoro <claudinemah3@gmail.com>
2020-11-10 02:12:53 +0200

    defining some services in the profile service.Ts file

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -5,6 +5,7 @@ import {ProfileService} from '../profile.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
 export class ProfileComponent implements OnInit {
   profile:any;
   repos:any;
